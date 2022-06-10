2e1493945b93894ee59ac6893dcfeb5281d9e4af
Pranith Hengavalli <pranith_h@yahoo.com.sg>
2022-06-10 23:40:08 +0530

    fuck vercel

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       
       <main className={styles.main}>
-        <img src={`${process.env.BASE_PATH}/logo.png`} />
+        <img src={`/logo.png`} />
         <h1 className={styles.title}>
           Walletspy
         </h1>
