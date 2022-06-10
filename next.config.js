98d6ea058aa7cc7220e81371fb56214b00a9d6e2
Pranith Hengavalli <pranith_h@yahoo.com.sg>
2022-06-10 22:59:38 +0530

    fix next export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/walletspy',
+  assetPrefix: '/walletspy',
   reactStrictMode: true,
   images: {
     loader: 'imgix',
     path: '',
   },
-  basePath: '/docs',
 }
 
 module.exports = nextConfig
